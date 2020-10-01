ed49de5e673e75c691f73216621fbf32a94625b9
Claudia <benko.claudia@yahoo.com>
2020-10-01 18:50:28 +0300

    added index files for modules - branch 03 alias

--- src/notes/NotesList.tsx
+++ src/notes/NotesList.tsx
@@ -1,8 +1,22 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon } from '@ionic/react';
+import { add } from 'ionicons/icons';
+import React, { useState } from 'react';
 import Note from './Note';
+import { getLogger } from '../core';
+
+const log = getLogger('ItemList')
 
 const Home: React.FC = () => {
+  const [items, setItems] = useState([
+    {id: '1', title: "first try", content: "hi"},
+    {id: '1', title: "it works", content: "yo"}
+  ])
+  const addItem = () => {
+    const id = `${items.length + 1}`;
+    log('ItemList addItem');
+    setItems(items.concat({id, title: `Title ${id}`, content: `Content ${id}`}))
+  }
+  log("ItemList render")
   return (
     <IonPage>
       <IonHeader>
@@ -11,8 +25,12 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <Note title="first try" content="hi" />
-        <Note title="it works" content="yo" />
+        {items.map(({ id, title, content}) => <Note key={id} title={title} content={content} />)}
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton onClick={addItem}>
+            <IonIcon icon={add} />
+          </IonFabButton>
+        </IonFab>
       </IonContent>
     </IonPage>
   );
