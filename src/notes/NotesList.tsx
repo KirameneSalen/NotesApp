3bdcf7f9758f7610c7c38003f82f6d7f7b19dd9f
KirameneSalen <pesticaEUnErou>
2020-11-29 17:21:43 +0200

    Added toggle favorite notes. bug remains

--- src/notes/NotesList.tsx
+++ src/notes/NotesList.tsx
@@ -7,9 +7,9 @@ import {
     IonFab,
     IonFabButton,
     IonIcon,
-    IonLoading, IonList, IonSearchbar
+    IonLoading, IonList, IonSearchbar, IonToggle, IonLabel, IonItem
 } from '@ionic/react';
-import { add } from 'ionicons/icons';
+import {add} from 'ionicons/icons';
 import React, {useContext, useState} from 'react';
 import Note from './Note';
 import { getLogger } from '../core';
@@ -21,6 +21,7 @@ const log = getLogger('NotesList')
 const NotesList: React.FC<RouteComponentProps> = ({ history }) => {
   const {notes, fetching, fetchingError} = useContext(NoteContext);
     const [searchNote, setSearchNote] = useState<string>('');
+    const [toggleFavNote, setToggleFavNote] = useState<boolean>(false);
   log("render")
   return (
     <IonPage>
@@ -30,15 +31,22 @@ const NotesList: React.FC<RouteComponentProps> = ({ history }) => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-          <IonSearchbar
-              value={searchNote}
-              debounce={1000}
-              onIonChange={e => setSearchNote(e.detail.value!)}>
-          </IonSearchbar>
+          <IonToolbar>
+              <IonSearchbar
+                  value={searchNote}
+                  debounce={1000}
+                  onIonChange={e => setSearchNote(e.detail.value!)}>
+              </IonSearchbar>
+              <IonItem>
+                  <IonToggle checked={toggleFavNote} onIonChange={e => setToggleFavNote(e.detail.checked)} slot={"start"}/>
+                  <IonLabel slot={"start"}>Toggle favorite</IonLabel>
+              </IonItem>
+          </IonToolbar>
           <IonLoading isOpen={fetching} message="Fetching notes" />
           {notes && (
               <IonList>
                   {notes
+                      .filter(note => note.favorite === toggleFavNote)
                       .filter(note => note.title.indexOf(searchNote) >= 0 || note.content.indexOf(searchNote) >= 0)
                       .map(({ _id, title, content, date, favorite}) =>
                       <Note key={_id} _id={_id} title={title} content={content} date={date} favorite={favorite} onEdit={id => history.push(`/note/${id}`)}/>)}
